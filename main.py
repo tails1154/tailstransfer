51cd0cc0ad26f79dc70fac72aff2476704003eab
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-11-02 17:48:07 -0600

    ok im already bored

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Tails1154 Data Transfer")
+print("Loading modules")
+import tkinter as tk
+print("Loaded modules!")
+print("Creating main window")
+root = tk.Tk()
+root.title("TailsTransfer")
+root.geometry("400x300")
+tk.Label(root, text="Welcome to tailstransfer! Please select a option").pack()
+
+root.mainloop()
